a6b2d81ffdd6b27ef54688ce7b0a3e1cba0eb192
Sahil Singh <kswayam2000@gmail.com>
2025-11-19 11:23:50 +0530

    add end and start game functions

--- Guess Game/app.js
+++ Guess Game/app.js
@@ -57,8 +57,36 @@ function checkGuess (guess) {
 
 function displayGuess(guess) {
     userInput.value = ''
-    guessSlot.innerHTML += `${guess}`
+    guessSlot.innerHTML += `${guess}, `
     numGuess++  
     remaining.innerHTML = `${11 - numGuess}`
 }
 
+function displayMessage(message) {
+    lowOrHi.innerHTML = `<h2>${message}</h2>`
+}
+
+function endGame(){
+    userInput.value = ''
+    userInput.setAttribute("disabled", "")
+    p.classList.add('button')
+    p.innerHTML = `<h2 id='newGame'>Start New Game</h2>`
+    startOver.appendChild(p)
+    playGame = false
+    newGame()
+}
+
+function newGame () {
+    const newGameBtn = document.querySelector("#newGame")
+    newGameBtn.addEventListener("click",(e) => {
+        randomNumber = parseInt((Math.random()*100) + 1)
+        prevGuess = []
+        numGuess = 1
+        guessSlot.innerHTML = ''
+        remaining.innerHTML = `${11 - numGuess}`;
+        userInput.removeAttribute('disabled')
+        startOver.removeChild(p)
+
+        playGame = true
+    })
+}

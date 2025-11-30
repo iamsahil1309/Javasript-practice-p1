c8fc8928a1e7fe3edc9edf6ec9b87e9f57202a3c
Sahil Singh <kswayam2000@gmail.com>
2025-11-30 15:50:22 +0530

    add snake

--- Snake Game/app.js
+++ Snake Game/app.js
@@ -1,13 +1,30 @@
-const board = document.querySelector('.board')
-const blockRow = 30
-const blockCol = 30
+const board = document.querySelector(".board");
+const blockRow = 30;
+const blockCol = 30;
 
-const rows = Math.floor(board.clientWidth/blockRow)
-const cols = Math.floor(board.clientHeight/blockRow)
+const rows = Math.floor(board.clientWidth / blockRow);
+const cols = Math.floor(board.clientHeight / blockRow);
 
-for(let i = 0; i < rows * cols; i++) {
-    const block = document.createElement('div')
-    block.classList.add('block')
-    board.appendChild(block)
+let blocks = [];
+let snake = [
+  { x: 1, y: 4 },
+  { x: 1, y: 5 },
+  { x: 1, y: 6 },
+];
+
+for (let row = 0; row < rows; row++) {
+  for (let col = 0; col < cols; col++) {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    board.appendChild(block);
+    // block.innerText = `${row}-${col}`;
+
+    blocks[`${row}-${col}`] = block;
+  }
 }
 
+const renderSnake = () => {
+  snake.forEach((box) => {
+    blocks[`${box.x}-${box.y}`].classList.add("fill");
+  });
+};

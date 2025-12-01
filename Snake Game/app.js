526304c93570c351d750ff8cee3d841ae9c5e615
Sahil Singh <kswayam2000@gmail.com>
2025-12-01 19:05:01 +0530

    add comments for better code read

--- Snake Game/app.js
+++ Snake Game/app.js
@@ -64,6 +64,7 @@ let food = {
 
 let direction = "right";
 
+// create blocks
 for (let row = 0; row < rows; row++) {
   for (let col = 0; col < cols; col++) {
     const block = document.createElement("div");
@@ -79,6 +80,7 @@ const renderSnake = () => {
 
   blocks[`${food.x} - ${food.y}`].classList.add("food");
 
+  // snake controls 
   if (direction === "left") {
     head = { x: snake[0].x, y: snake[0].y - 1 };
   } else if (direction === "right") {
@@ -114,7 +116,7 @@ const renderSnake = () => {
     startGameModal.style.display = "none";
     gameOverModal.style.display = "flex";
   }
-
+  
   snake.forEach((box) => {
     blocks[`${box.x} - ${box.y}`].classList.remove("fill");
   });

ce0b2659bafd7c20ce569d9bb9440b3c1247e6d1
Sahil Singh <kswayam2000@gmail.com>
2025-12-02 12:44:09 +0530

    drag and drop functionality

---
+++ D&D Todo Board/app.js
@@ -0,0 +1,41 @@
+const todo = document.getElementById("todo") 
+const progress = document.getElementById("progress") 
+const done = document.getElementById("done") 
+const tasks = document.querySelectorAll(".task")
+const modal = document.querySelector('.modal')
+const toggleModal = document.getElementById("toggle-task")
+
+let dropElement 
+
+tasks.forEach((task) => {
+    task.addEventListener("drag", () => {
+        dropElement = task
+    })
+})
+
+const dragAndDropTask = (taskCol) => {
+    taskCol.addEventListener("dragenter", (e) => {
+        e.preventDefault()
+        taskCol.classList.add("hover-over")
+    })
+    taskCol.addEventListener("dragleave", (e) => {
+        e.preventDefault()
+        taskCol.classList.remove("hover-over")
+    })
+    taskCol.addEventListener("dragover", (e) => {
+        e.preventDefault()
+    })
+    taskCol.addEventListener("drop", (e) => {
+        e.preventDefault()
+        taskCol.appendChild(dropElement)
+        taskCol.classList.remove("hover-over")
+    })
+}
+
+dragAndDropTask(todo) 
+dragAndDropTask(progress) 
+dragAndDropTask(done) 
+
+toggleModal.addEventListener("click", () => {
+    modal.classList.add("active")
+})

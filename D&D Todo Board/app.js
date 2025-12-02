ffcb8bba70437235c857fdcf415952e70f5337a4
Sahil Singh <kswayam2000@gmail.com>
2025-12-02 17:33:35 +0530

    add and drag new tasks

--- D&D Todo Board/app.js
+++ D&D Todo Board/app.js
@@ -1,46 +1,66 @@
-const todo = document.getElementById("todo") 
-const progress = document.getElementById("progress") 
-const done = document.getElementById("done") 
-const tasks = document.querySelectorAll(".task")
-const modal = document.querySelector('.modal')
-const toggleModal = document.getElementById("toggle-task")
-const modalBg = document.querySelector(".bg")
+const todo = document.getElementById("todo");
+const progress = document.getElementById("progress");
+const done = document.getElementById("done");
+const tasks = document.querySelectorAll(".task");
+const modal = document.querySelector(".modal");
+const toggleModal = document.getElementById("toggle-task");
+const modalBg = document.querySelector(".bg");
+const addTask = document.getElementById("add-new-task");
 
-let dropElement 
-
-tasks.forEach((task) => {
-    task.addEventListener("drag", () => {
-        dropElement = task
-    })
-})
-
-const dragAndDropTask = (taskCol) => {
-    taskCol.addEventListener("dragenter", (e) => {
-        e.preventDefault()
-        taskCol.classList.add("hover-over")
-    })
-    taskCol.addEventListener("dragleave", (e) => {
-        e.preventDefault()
-        taskCol.classList.remove("hover-over")
-    })
-    taskCol.addEventListener("dragover", (e) => {
-        e.preventDefault()
-    })
-    taskCol.addEventListener("drop", (e) => {
-        e.preventDefault()
-        taskCol.appendChild(dropElement)
-        taskCol.classList.remove("hover-over")
-    })
-}
-
-dragAndDropTask(todo) 
-dragAndDropTask(progress) 
-dragAndDropTask(done) 
+let dropElement;
 
 toggleModal.addEventListener("click", () => {
-    modal.classList.add("active")
-})
+  modal.classList.add("active");
+});
 
 modalBg.addEventListener("click", () => {
-    modal.classList.remove("active")
-})
+  modal.classList.remove("active");
+});
+
+addTask.addEventListener("click", () => {
+  const taskInput = document.getElementById("input").value;
+  const taskText = document.getElementById("text-input").value;
+
+  const div = document.createElement("div");
+  div.classList.add("task");
+  div.setAttribute("draggable", "true");
+  div.innerHTML = `<h2>${taskInput}</h2>
+                    <p>${taskText}</p>
+                    <button>Delete</button>`;
+  todo.appendChild(div);
+
+  div.addEventListener("drag", () => {
+    dropElement = div;
+  });
+
+  modal.classList.remove("active");
+});
+
+tasks.forEach((task) => {
+  task.addEventListener("drag", () => {
+    dropElement = task;
+  });
+});
+
+const dragAndDropTask = (taskCol) => {
+  taskCol.addEventListener("dragenter", (e) => {
+    e.preventDefault();
+    taskCol.classList.add("hover-over");
+  });
+  taskCol.addEventListener("dragleave", (e) => {
+    e.preventDefault();
+    taskCol.classList.remove("hover-over");
+  });
+  taskCol.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  });
+  taskCol.addEventListener("drop", (e) => {
+    e.preventDefault();
+    taskCol.appendChild(dropElement);
+    taskCol.classList.remove("hover-over");
+  });
+};
+
+dragAndDropTask(todo);
+dragAndDropTask(progress);
+dragAndDropTask(done);
